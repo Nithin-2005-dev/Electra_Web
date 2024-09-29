e7df7ab5173a0c67ea3800f4449774d069c3e310
[Nithin-2005-dev] <bingu_ug_23@ee.nits.ac.in>
2024-09-29 23:18:28 +0530

    team page added

--- electra-user-portal/src/components/coreTeamFilter/CoreTeamFilter.js
+++ electra-user-portal/src/components/coreTeamFilter/CoreTeamFilter.js
@@ -1,18 +1,16 @@
 'use client'
+import styles from '../../app/styles/CoreTeam.module.css'
 import React from 'react'
-import styles from '../../app/styles/ImageFilter.module.css'
-const ImageFilter = () => {
+const CoreTeamFilter = () => {
   return (
-    <div className='my-5 mx-3 flex justify-between border-1 p-2 rounded-xl text-xs lg:text-xl font-bold shadow-xl shadow-slate-800'>
-      <div>
-        <select name="" id="" className='bg-slate-600 rounded-xl hover:opacity-80 lg:p-2 lg:px-3 md:p-2 md:px-3 sm:p-3 sm:px-3 p-1 px-1 text-xs lg:text-xl font-bold'>
+    <div className='m-3'>
+      <div className={`text-center text-3xl text-fuchsia-400 [text-shadow:_0_0rem_1rem_rgb(99_102_255_/_0.8)] ${styles['permanent-marker-regular']}`}>Meet Our Team</div>
+      <div className='flex justify-end my-2'>
+        <select name="" id="" className='bg-blue-400 rounded-xl hover:opacity-80 lg:p-2 lg:px-3 md:p-3 md:px-3 sm:p-3 sm:px-3 p-0.5 px-0.5 m-2 font-bold text-sm'>
             <option value="" className='bg-slate-500'>2023-2024</option>
             <option value="" className='bg-slate-500'>2022-2023</option><option value="" className='bg-slate-500'>2021-2022</option><option value="" className='bg-slate-500'>2020-2021</option><option value="" className='bg-slate-500'>2019-2020</option>
         </select>
-      </div>
-      <div className={`${styles['permanent-marker-regular']} text-blue-200 text-base lg:text-2xl md:text-lg`}>Electra Gallery</div>
-      <div>
-      <select name="" id="" className='bg-slate-600 rounded-xl hover:opacity-80 lg:p-2 lg:px-3 md:p-2 md:px-3 sm:p-3 sm:px-3 p-1 px-1 text-xs lg:text-xl font-bold'>
+        <select name="" id="" className='bg-blue-400 rounded-xl hover:opacity-80 lg:p-2 lg:px-3 md:p-3 md:px-3 sm:p-3 sm:px-3 p-0.5 px-0.5 m-2 font-bold text-sm'>
             <option value="" className='bg-slate-500'>Tech</option>
             <option value="" className='bg-slate-500'>Social</option><option value="" className='bg-slate-500'>Content</option><option value="" className='bg-slate-500'>Design</option><option value="" className='bg-slate-500'>Marketing</option>
         </select>
@@ -21,4 +19,4 @@ const ImageFilter = () => {
   )
 }
 
-export default ImageFilter
+export default CoreTeamFilter
