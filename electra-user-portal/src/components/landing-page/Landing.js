6fd4954f2f397e0eb2785a1f61b9c329e14549a9
[Nithin-2005-dev] <nithinkumar2005b@gmail.com>
2024-10-01 18:12:19 +0530

    ui fixed

--- electra-user-portal/src/components/landing-page/Landing.js
+++ electra-user-portal/src/components/landing-page/Landing.js
@@ -57,8 +57,8 @@ const Landing = () => {
           delay:1,
         }
       }}>The Official Society of the Electrical Engineering Department, NIT Silchar</motion.div>
-      <div className=' absolute bottom-[18vh]'>
-      <Link href={'/pages/Sign-Up'} className='bg-yellow-600 font-black px-3 sm:text-xl rounded-xl mr-2 py-1 text-sm hover:scale-110 hover:bg-yellow-500 hover:border-4 hover:border-double'>
+      <div className=' absolute bottom-[10vh] md:bottom-[18vh]'>
+      <Link href={'/pages/Sign-Up'} className='bg-yellow-600 font-black px-3 sm:text-2xl rounded-xl mr-2 py-1 text-lg  hover:scale-110 hover:bg-yellow-500 hover:border-4 hover:border-double'>
       Join Us
     </Link>
       </div>
