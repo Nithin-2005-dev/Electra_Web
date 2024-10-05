a559fab16590c167c82ffd1a591339d640f793e7
[Nithin-2005-dev] <nithinkumar2005b@gmail.com>
2024-10-05 20:39:21 +0530

    deployment error fixed

--- electra-user-portal/src/app/external_apis/genAi.js
+++ electra-user-portal/src/app/external_apis/genAi.js
@@ -1,5 +1,5 @@
 import {GoogleGenerativeAI} from '@google/generative-ai'
-export const genAi=new GoogleGenerativeAI('AIzaSyDIXTTI0hJq-WkXR-hVUariihUJO7frU_0');
+export const genAi=new GoogleGenerativeAI(process.env.GENAI_API_KEY);
 const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
 export const askApi= async(prompt)=>{
     const result = await model.generateContent(prompt);
