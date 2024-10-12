456b3ad5a61f91f2d89d989f6643e075a623599d
[Nithin-2005-dev] <nithinkumar2005b@gmail.com>
2024-10-12 14:09:26 +0530

    ...

--- electra-user-portal/src/components/cardBorder/TeamCardBorder.jsx
+++ electra-user-portal/src/components/cardBorder/TeamCardBorder.jsx
@@ -23,12 +23,11 @@ const StyledWrapper = styled.div`
  font-size: 18px;
  font-weight: 500;
  color: #fff;
- z-index: -200;
+ z-index: 0;
  border-radius: 15px;
 }
 
 .card::after {
-	z-index: -200;
  --m-i: linear-gradient(#000, #000);
  --m-o: content-box, padding-box;
  content: "";
