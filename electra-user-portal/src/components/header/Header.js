99a55b427b8282e69e8aa1f438a7a09324fc0a85
[Nithin-2005-dev] <nithinkumar2005b@gmail.com>
2024-10-18 17:21:33 +0530

    ..

--- electra-user-portal/src/components/header/Header.js
+++ electra-user-portal/src/components/header/Header.js
@@ -97,7 +97,7 @@ const Header = () => {
     </Link>
         }
         {
-            user && <div className='bg-red-400 rounded-xl text-center hover:opacity-75 mx-2 border-gray-100 hover:border-2 border'>
+            user && <div className='bg-red-600 rounded-xl text-center hover:opacity-75 mx-2 border-gray-100 hover:border-2 border'>
         <SignOutButton>
             log out
         </SignOutButton>
